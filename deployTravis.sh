620c76217c9860f44784bab2dcf82ed5519bae4f
Travis-CI <travis@nelsonauner.com>
2015-04-30 18:21:29 +0000

    echo environmental variables to solve URL spec issue

--- deployTravis.sh
+++ deployTravis.sh
@@ -1,4 +1,8 @@
+echo ${GH_TOKEN}
+echo ${GH_REF}
+
+git checkout gh-pages
 git config user.name "Travis-CI"
 git config user.email "travis@nelsonauner.com"
 git commit -a -m 'travis deployment to github pages'
-git push --quiet "https://nelsonauner:${GH_TOKEN}@${GH_REF}"
+git push --quiet "https://nelsonauner:${GH_TOKEN}@${GH_REF}" gh-pages
