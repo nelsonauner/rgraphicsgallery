e5227e23c598e8726ee55373b5d9dda2b1d80299
Travis-CI <travis@nelsonauner.com>
2015-04-30 19:36:20 +0000

    travis deployment to github pages

--- deployTravis.sh
+++ deployTravis.sh
@@ -3,5 +3,4 @@ echo ${GH_REF}
 git config user.name "Travis-CI"
 git config user.email "travis@nelsonauner.com"
 git commit -a -m 'travis deployment to github pages'
-
-git push --quiet "https://nelsonauner:${GH_TOKEN}@${GH_REF}"
+git push --quiet "https://nelsonauner:${GH_TOKEN}@${GH_REF}" gh-pages
