44e880f76b87cd652c9d85ad743556442964d81f
Nelson Auner <nelson@nelsonauner.com>
2015-05-02 12:54:57 -0600

    try using http://www.steveklabnik.com/automatically_update_github_pages_with_travis_example/"

--- deployTravis.sh
+++ deployTravis.sh
@@ -1,10 +1,13 @@
-echo ${GH_TOKEN}
-echo ${GH_REF}
-git status
+set -o errexit -o nounset
+rev=$(git rev-parse --short HEAD)
+git init
 git config user.name "Travis-CI"
 git config user.email "travis@nelsonauner.com"
-git add --all --force
-git status
-git commit -m 'travis deployment to github pages'
-git status
-git push --verbose "https://nelsonauner:${GH_TOKEN}@${GH_REF}" master
+git remote add upstream "https://nelsonauner:${GH_TOKEN}@${GH_REF}"
+git fetch upstream 
+git reset upstream/gh-pages
+echo ${GH_TOKEN}
+echo ${GH_REF}
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
