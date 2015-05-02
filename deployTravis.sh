c1800d33ac0e51196305103a0c672268d23a17ed
Travis-CI <travis@nelsonauner.com>
2015-05-01 22:40:49 -0500

    deploy

--- deployTravis.sh
+++ deployTravis.sh
@@ -4,5 +4,7 @@ git status
 git config user.name "Travis-CI"
 git config user.email "travis@nelsonauner.com"
 git add --all
+git status
 git commit -m 'travis deployment to github pages'
+git status
 git push --verbose "https://nelsonauner:${GH_TOKEN}@${GH_REF}" master
